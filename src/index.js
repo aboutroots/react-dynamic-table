c7db529bfbfe848d8d795a5c66993ac0a5d4fc16
aboutroots <popiel.mw@gmail.com>
2019-04-19 08:15:06 +0200

    Configure redux store and set up base reducers

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+const jsx = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+ReactDOM.render(jsx, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
